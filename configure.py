d33d3b2266672c82dd6cacde02e3d223602ba6fd
Amit verma <av553202@gmail.com>
2025-07-12 00:14:15 +0530

    Database-changes

--- configure.py
+++ configure.py
@@ -3,8 +3,8 @@ import pymysql
 
 def get_db_connection():
     return pymysql.connect(
-        host="employ-database.c2rsg2uwe0k9.us-east-1.rds.amazonaws.com",
-        user="admin",
+        host="employee.c2rsg2uwe0k9.us-east-1.rds.amazonaws.com",
+        user="amitverma",
         password="Rajesh1234",
         database="employee"
     )
