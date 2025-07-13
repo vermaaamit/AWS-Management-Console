c0b4f889b2c2139987edabda1a57b4d76afd2ca1
Amit verma <110997038+vermaaamit@users.noreply.github.com>
2025-07-13 17:06:54 +0530

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,6 +1,1 @@
-custombucket = "av553202"
-customregion = "us-east-1"  # e.g., us-east-1
-customhost = "employee.c2rsg2uwe0k9.us-east-1.rds.amazonaws.com"
-customuser = "amitverma"
-custompass = "Rajesh1234"
-customdb = "employee"
+
